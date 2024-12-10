21dbb143e29aeeb6bb48555bb8817dd4676fceaa
kimyou1102 <yoosk5485@naver.com>
2024-12-10 17:45:59 +0900

    feat: 식당 예상 방문 날짜 입력 받는 기능 구현

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,7 +1,9 @@
+import InputView from '../view/InputView.js';
 import OutputView from '../view/OutputView.js';
 
 export default class Controller {
   async start() {
     OutputView.printGreeting();
+    const visitDate = await InputView.readDate();
   }
 }

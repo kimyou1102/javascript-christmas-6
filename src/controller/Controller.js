c95f5190807aeae35382e9fc3f69feb6f6d92403
kimyou1102 <yoosk5485@naver.com>
2024-12-10 17:50:18 +0900

    feat: 주문할 메뉴와 수량 입력받는 기능 구현
    - 입력 받은 문자열을 이름과 수량을 가진 객체배열로 변환해주는 파서 함수 생성

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,9 +1,12 @@
 import InputView from '../view/InputView.js';
 import OutputView from '../view/OutputView.js';
+import { parseMenus } from '../utils/parseMenus.js';
 
 export default class Controller {
   async start() {
     OutputView.printGreeting();
     const visitDate = await InputView.readDate();
+    const orderMenu = await InputView.readMenu();
+    const menu = parseMenus(orderMenu);
   }
 }
